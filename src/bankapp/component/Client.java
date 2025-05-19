b32d45a43d1226fa7f82eebc6376a2c7b3b79be7
RamziMaoua <ramzi.maoua@gmail.com>
2025-05-19 14:24:35 +0200

    1. Clients exercise commit

--- src/bankapp/component/Client.java
+++ src/bankapp/component/Client.java
@@ -1,5 +1,54 @@
 package bankapp.component;
 
+//--------1.1.1 Creation of the Client Class
 public class Client {
+	
+	private String firstName;
+	private String name;
+	private int clientNumber;
+	
+	//numberOfClient counts
+	private static int numberOfClients;
+	
+	
+	public Client(String name, String firstName) {
+		this.firstName = firstName;
+		this.name = name;
+		numberOfClients++;
+		this.clientNumber = numberOfClients;
+	}
+	
+	
+	public String getFirstName() {
+		return this.firstName;
+	}	
+	
+	public void setFirstName (String firstName) {
+		this.firstName = firstName;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName (String name) {
+		this.name = name;
+	}
+
+	public int getClientNumber() {
+		return clientNumber;
+	}
+	
+	public void setClientNumber (int clientNumber) {
+		this.clientNumber = clientNumber;
+	}
+	
+	public static int getNumberOfClients() {
+		return numberOfClients;
+	}
+	
+	
+	public String toString() {
+		return "(Client number: " + getClientNumber() + ", Name: " + getName() + ", First Name: " + getFirstName() + ")" ;
+	}
 }
--- src/bankapp/main/Main.java
+++ src/bankapp/main/Main.java
@@ -1,10 +1,48 @@
 package bankapp.main;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import bankapp.component.Client;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//-------- 1.1.2 Creation of the main class for tests
+				Scanner scanner = new Scanner(System.in);
+				System.out.println("How many clients will use our bank ? \t" );
+				int numberOfClients = scanner.nextInt();
+				scanner.close();
+				
+				ArrayList<Client> clientCollection = new ArrayList<>();
+				clientCollection = generateClientCollection(numberOfClients);
+				displayClients(clientCollection);
+				
 	}
-
+	
+	
+	//----------- 1.1.2 Creation of the main class for tests
+		public static ArrayList<Client> generateClientCollection(int numberOfClients) {
+			ArrayList<Client> collection = new ArrayList<Client>(numberOfClients);
+			
+			for (int i= 0; i< numberOfClients; i++) {
+				collection.add(i, new Client("name" + (i + 1), "firstname" + (i + 1)));
+			}
+			
+			return collection;
+		}
+		
+		public static ArrayList<String>  convertClientCollectionToString(List<Client> clients) {
+			int numberOfClients = clients.size();
+			ArrayList<String> collectionToString = new ArrayList<>(numberOfClients);
+			for (int i = 0; i < numberOfClients; i++) {
+				collectionToString.add(i, (clients.get(i).toString()));
+			}
+			return collectionToString;	
+		}
+		
+		public static void displayClients(List<Client> clients) {
+			System.out.println(convertClientCollectionToString(clients));
+		}
 }
